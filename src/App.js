775339752c56b731af74458b39d70b9c4a98abf7
aprilyanggarwood <yangyyj822@hotmail.com>
2021-03-18 04:07:55 -0700

    created sidenav bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Nav from "./components/Nav/Navbar";
+import About from "./components/pages/About";
+import Portfolio from "./components/pages/Portfolio";
+import Contact from "./components/pages/Contact";
+// import "./App.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Nav />
+        <Switch>
+          <Route path="/" exact component={About} />
+          <Route path="/portfolio" component={Portfolio} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/Nav/SidebarData.js
@@ -0,0 +1,26 @@
+import React from "react";
+// import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import * as MdIcons from "react-icons/md";
+import * as BsIcons from "react-icons/bs";
+
+export const SidebarData = [
+  {
+    title: "About",
+    path: "/",
+    icon: <BsIcons.BsPeopleCircle />,
+    cName: "nav-text",
+  },
+  {
+    title: "Portfolio",
+    path: "/portfolio",
+    icon: <AiIcons.AiOutlineFundProjectionScreen />,
+    cName: "nav-text",
+  },
+  {
+    title: "Contact",
+    path: "/contact",
+    icon: <MdIcons.MdContactMail />,
+    cName: "nav-text",
+  },
+];
